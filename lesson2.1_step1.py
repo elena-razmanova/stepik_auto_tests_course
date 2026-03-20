7814078bccb5170e35225ac97ffe8a75ab9db3f7
Elena Razmanova <elenarazmanova33580@gmail.com>
2026-03-20 15:57:07 +0300

    кликаем по checkboxes и radiobuttons

---
+++ lesson2.1_step1.py
@@ -0,0 +1,45 @@
+import math
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try: 
+    link = "https://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Считать значение для переменной x
+    def calc(x):
+        return str(math.log(abs(12*math.sin(int(x)))))
+
+    # Посчитать математическую функцию от x 
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)     
+    
+    # Ввести ответ в текстовое поле
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+    
+    # Отметить checkbox "I'm the robot"
+    option1 = browser.find_element(By.ID, "robotCheckbox")
+    option1.click()
+    
+    # Выбрать radiobutton "Robots rule!
+    option2 = browser.find_element(By.ID, "robotsRule")
+    option2.click()
+    
+    # Нажать на кнопку Submit
+    button = browser.find_element(By.CSS_SELECTOR, "[type='submit']")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
